526d83b32a5b1668a9b1f5446abc5065ae4d914e
Maciej Gawel <m.gawel@hotmail.com>
2016-06-05 00:46:42 +0200

    Add credits to main menu

--- Assets/Scripts/UI/MenuScript.cs
+++ Assets/Scripts/UI/MenuScript.cs
@@ -6,18 +6,24 @@ using System.Collections.Generic;
 public class MenuScript : MonoBehaviour {
 
     public Canvas quitMenu;
+    public Canvas credits;
     public Button multiplayerText;
     public Button singleplayerText;
     public Button exitText;
 
+    Canvas menu;
+    bool creditsEnabled = false;
+
 	void Start ()
     {
+        menu = this.GetComponent<Canvas>();
         quitMenu = quitMenu.GetComponent<Canvas>();
         multiplayerText = multiplayerText.GetComponent<Button>();
         singleplayerText = singleplayerText.GetComponent<Button>();
         exitText = exitText.GetComponent<Button>();
 
         quitMenu.enabled = false;
+        credits.enabled = false;
 	}
 
     public void ExitPress()
@@ -46,6 +52,20 @@ public class MenuScript : MonoBehaviour {
     {
         GameManager.SetGameType(GameType.singleplayer);
         SceneManager.LoadScene("Sandbox");
+    }
+
+    public void ToggleCredits()
+    {
+        if (!creditsEnabled)
+        {
+            menu.enabled = false;
+            credits.enabled = creditsEnabled = true;
+        }
+        else
+        {
+            menu.enabled = true;
+            credits.enabled = creditsEnabled = false;
+        }
     }
 
     public void ExitGame()
