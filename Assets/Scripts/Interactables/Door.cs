a900dc7f0325765b2ff009d0eb19ba19f9c6e2f2
Maciej Gawel <m.gawel@hotmail.com>
2016-06-06 16:06:22 +0200

    Door related cleanups

---
+++ Assets/Scripts/Interactables/Door.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+    public float doorSpeed = 10f;
+
+    bool isOpened = true;
+    int playersInRange = 0;
+
+    void Awake()
+    {
+    }
+
+    void Update()
+    {
+        if (playersInRange > 0 && (Input.GetButtonDown("Action_1") || Input.GetButtonDown("Action_2")))
+        {
+            isOpened = !isOpened;
+        }
+        Move();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playersInRange++;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            playersInRange--;
+        }
+    }
+
+    void Move()
+    {
+        if (isOpened)
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0f, 0f, 0f), Time.deltaTime * doorSpeed);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0f, 90f, 0f), Time.deltaTime * doorSpeed);
+        }
+    }
+}
